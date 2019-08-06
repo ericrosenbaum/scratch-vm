00d77b4db3999b60e59755cc82e7b8cbc807a857
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2019-08-06 11:25:17 -0400

    Always round loudness

--- src/extensions/scratch3_sound_sensing/Loudness.js
+++ src/extensions/scratch3_sound_sensing/Loudness.js
@@ -65,7 +65,7 @@ class Loudness {
             let loudness = this.getLoudnessOfArray(this.micDataArray);
             // smooth the value, if it is descending
             if (this._lastMicLoudness) {
-                loudness = Math.max(loudness, this._lastMicLoudness * SMOOTHING);
+                loudness = Math.round(Math.max(loudness, this._lastMicLoudness * SMOOTHING));
             }
             this._lastMicLoudness = loudness;
             return loudness;
@@ -85,7 +85,7 @@ class Loudness {
         let loudness = this.getLoudnessOfArray(this.projectDataArray);
         // smooth the value, if it is descending
         if (this._lastProjectLoudness) {
-            loudness = Math.max(loudness, this._lastProjectLoudness * SMOOTHING);
+            loudness = Math.round(Math.max(loudness, this._lastProjectLoudness * SMOOTHING));
         }
         this._lastProjectLoudness = loudness;
         return loudness;
