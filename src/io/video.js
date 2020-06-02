82d38ac239131b178bbe1438d144644d5beb0b84
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2020-06-02 17:27:53 -0400

    disable ghost and enable color effect for video

--- src/io/video.js
+++ src/io/video.js
@@ -157,7 +157,8 @@ class Video {
         // if we haven't already created and started a preview frame render loop, do so
         if (!this._renderPreviewFrame) {
             renderer.updateDrawableProperties(this._drawable, {
-                ghost: this._forceTransparentPreview ? 100 : this._ghost,
+                // ghost: this._forceTransparentPreview ? 100 : this._ghost,
+                color: 100,
                 visible: true
             });
 
